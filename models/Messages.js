216186f4b7086a0417ea1d5a9c06199cdb298aaa
kuba03070-a11y <kuba03070@gmail.com>
2026-01-19 14:19:29 +0100

    Create Messages.js

---
+++ models/Messages.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const Message = sequelize.define('Message', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        channelId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    });
+
+    return Message;
+};
