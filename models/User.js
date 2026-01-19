a0cad17854223673eb1fe2fd2ea706d3631f9092
kuba03070-a11y <kuba03070@gmail.com>
2026-01-19 14:18:18 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        avatar: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+
+    return User;
+};
